2aeee3e29860e2a95ef293063d1fa5862baa337c
공채원 <chaewonkong@gmail.com>
2018-08-26 22:04:09 +0900

    Add Dimensions and shadows

--- react-native/baegopa/App.js
+++ react-native/baegopa/App.js
@@ -1,16 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  StatusBar,
+  Platform
+} from "react-native";
+import TabMenu from "./TabMenu";
+
+const { width, height } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar barStyle={"light-content"} />
         <View style={styles.header}>
-          <Text style={{ color: "white" }}>Titles & Filters Here</Text>
+          <Text style={styles.text}>달빛창가 302호</Text>
         </View>
-        <Image source={require("./img/2.png")} style={styles.imgStyle} />
+        <TouchableOpacity style={styles.card}>
+          <Image source={require("./img/2.png")} style={styles.imgStyle} />
+        </TouchableOpacity>
         <View style={styles.footer}>
-          <Text>Tab Menus Here</Text>
+          <TabMenu />
         </View>
       </View>
     );
@@ -31,11 +46,33 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
+  text: {
+    marginTop: 20,
+    fontSize: 26,
+    color: "white",
+    fontWeight: "500"
+  },
+  card: {
+    flex: 6,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 1
+      }
+    })
+  },
   imgStyle: {
+    flex: 1,
     marginTop: 30,
-    marginBottom: 30,
-    flex: 5,
-    width: 330
+    width: width - 50
   },
   footer: {
     flex: 1,
