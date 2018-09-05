dd9384064389acd6bdcd254cf2084a5e58bfe4bb
공채원 <chaewonkong@gmail.com>
2018-09-05 15:49:01 +0900

    Create like/dislike button to save action

--- react-native/baegopa/App.js
+++ react-native/baegopa/App.js
@@ -10,20 +10,47 @@ import {
   Platform
 } from "react-native";
 import Navigation from "./components/Navigation";
+import uuidv1 from "uuid/v1";
+import { FontAwesome } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
-const foodData = {
-  name: "달빛창가 302호",
-  img: require("./img/2.png")
-};
+const foodData = [
+  {
+    name: "흑돼지 삼겹살",
+    img: require("./img/1.png"),
+    status: ""
+  },
+  {
+    name: "달빛창가 302호",
+    img: require("./img/2.png"),
+    status: ""
+  },
+  {
+    name: "오가네 갈비",
+    img: require("./img/3.png"),
+    status: ""
+  },
+  {
+    name: "과일빙수",
+    img: require("./img/4.png"),
+    status: ""
+  },
+  {
+    name: "고등어회",
+    img: require("./img/5.png"),
+    status: ""
+  }
+];
 
 export default class App extends React.Component {
   state = {
-    name: foodData.name,
-    img: foodData.img
+    id: 0,
+    liked: [],
+    disliked: []
   };
   render() {
-    const { name, img } = this.state;
+    const { id } = this.state;
+    const { name, img } = foodData[id];
     return (
       <View style={styles.container}>
         <StatusBar barStyle={"light-content"} />
@@ -36,9 +63,57 @@ export default class App extends React.Component {
         <View style={styles.floatButton}>
           <Navigation />
         </View>
+        <View style={styles.action}>
+          <TouchableOpacity onPressOut={this._like}>
+            <FontAwesome name={"heart"} size={50} style={styles.like} />
+          </TouchableOpacity>
+          <TouchableOpacity onPressOut={this._dislike}>
+            <FontAwesome name={"close"} size={50} style={styles.dislike} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
+  _like = () => {
+    console.log(this.state.liked);
+    this.setState(prevState => {
+      if (prevState.id < foodData.length - 1) {
+        const newState = {
+          ...prevState,
+          id: prevState.id + 1,
+          liked: [...prevState.liked, prevState.id]
+        };
+        return newState;
+      } else {
+        const newState = {
+          ...prevState,
+          id: 0,
+          liked: [...prevState.liked, prevState.id]
+        };
+        return newState;
+      }
+    });
+  };
+  _dislike = () => {
+    console.log(this.state.disliked);
+    this.setState(prevState => {
+      if (prevState.id < foodData.length - 1) {
+        const newState = {
+          ...prevState,
+          id: prevState.id + 1,
+          disliked: [...prevState.disliked, prevState.id]
+        };
+        return newState;
+      } else {
+        const newState = {
+          ...prevState,
+          id: 0,
+          disliked: [...prevState.disliked, prevState.id]
+        };
+        return newState;
+      }
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -49,7 +124,7 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   header: {
-    flex: 1,
+    flex: 3,
     width: "100%",
     backgroundColor: "#d32323",
     alignItems: "center",
@@ -62,8 +137,8 @@ const styles = StyleSheet.create({
     fontWeight: "500"
   },
   card: {
-    flex: 7,
-    marginBottom: 30,
+    marginBottom: 20,
+    flex: 16,
     ...Platform.select({
       ios: {
         shadowColor: "rgb(50, 50, 50)",
@@ -82,12 +157,26 @@ const styles = StyleSheet.create({
   imgStyle: {
     flex: 1,
     marginTop: 20,
-    marginBottom: 20,
     width: width - 20
   },
   floatButton: {
     position: "absolute",
-    top: height - 120,
+    top: height - 170,
     left: width - 80
+  },
+  action: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  like: {
+    marginLeft: 20,
+    marginRight: 20,
+    color: "green"
+  },
+  dislike: {
+    marginLeft: 20,
+    marginRight: 20,
+    color: "red"
   }
 });
