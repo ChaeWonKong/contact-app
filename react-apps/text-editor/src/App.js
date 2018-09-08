af8eb9e34501642e96865e525d174a419870d2f5
공채원 <chaewonkong@gmail.com>
2018-09-08 11:52:25 +0900

    Separate title input as new component

--- react-apps/text-editor/src/App.js
+++ react-apps/text-editor/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import TitleForm from "./components/TitleForm";
 
 class App extends Component {
+  state = {
+    post: {
+      title: "",
+      content: ""
+    }
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <div className="editor" />
+        <div className="contents">
+          <TitleForm />
+          <textarea className="conText" placeholder="Write Text Here!" />
+        </div>
+        <div className="submit" />
+      </Fragment>
     );
   }
+  handleChange = e => {
+    this.setState({
+      [e.target.title]: e.target.value
+    });
+  };
 }
 
 export default App;
---
+++ react-apps/text-editor/src/components/TitleForm.js
@@ -0,0 +1,7 @@
+import React, { Component, Fragment } from "react";
+
+export default class TitleForm extends Component {
+  render() {
+    return <input className="titleText" type="text" placeholder="Title" />;
+  }
+}
