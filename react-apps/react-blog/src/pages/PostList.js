638964b5c140600f0a712721bc61518f38b4620f
공채원 <chaewonkong@gmail.com>
2018-10-13 22:38:59 +0900

    Add About to pages

---
+++ react-apps/react-blog/src/pages/PostList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PostDetail extends Component {
+  render() {
+    return (
+      <div>
+        <p>this is a Post detail</p>
+      </div>
+    );
+  }
+}
