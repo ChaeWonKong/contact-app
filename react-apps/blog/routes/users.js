838c0b48fd23b75cd7b8926b02072520f8f1a30b
공채원 <chaewonkong@gmail.com>
2018-10-23 12:43:42 +0900

    Change routes/users

--- react-apps/blog/routes/users.js
+++ react-apps/blog/routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  // res.send('respond with a resource');
+
+  res.json([
+    {
+      id: 1,
+      username: "leonkong"
+    },
+    {
+      id: 2,
+      username: "Sana"
+    }
+  ]);
 });
 
 module.exports = router;
